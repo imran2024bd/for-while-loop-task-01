cb55a86d0375b4d34d5960715dc4881d2da4f9bd
imran2024bd <imranitbd34@gmail.com>
2024-01-28 11:42:53 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,58 @@
+// const number = 7;
+
+// if (number < 10) {
+//     console.log(' number is smaller than 10');
+// }
+// console.log(' to write a new number');
+
+// if (number < 10) {
+//     console.log(' number is smaller than 10');
+// }
+// console.log(' to write a new number');
+
+//------------While ---Loop______________________
+// let num = 0;  // loop variable
+// while (num < 5) {
+//     console.log('looping' , num);
+//     // num = num + 1 ;
+//     // num += 1;
+//     num++;
+// }
+// to write down to 10
+// let num = 1;
+// while (num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// to write down 50 to 100
+// let num = 50;
+// while (num <= 100) {
+//     console.log(num);
+//     num++;
+// }
+
+// addition the number of 1 to 10 by while loop
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 10) {
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum : ', sum);
+//     num++;
+// }
+
+
+//*************************************** */
+
+// addition the number of even number by while loop
+
+let num = 1;
+while (num <=10) {
+    console.log(num);
+    if (num % 2 ===0) {
+        console.log('even number :', num);
+    }
+    num++;
+}
