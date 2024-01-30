fcccb1ad7c98f84fc84165c2ca0618f73287c8da
imran2024bd <imranitbd34@gmail.com>
2024-01-30 12:46:29 +0600

    for loop task odd & even number done

---
+++ practise-for.js
@@ -0,0 +1,48 @@
+//*********************************************************** */
+
+// ----for ---Loop
+// number of 1 to 5 by for loop
+
+// for (let num = 0; num <= 5; num++) {
+//     console.log(num);
+    
+// }
+
+
+//*********************************************************** */
+// number of 1 to 10 by for loop
+
+for ( let i = 1; i <=10 ; i++){
+    console.log(i);
+}
+
+
+//*********************************************************** */
+
+// for even number  print
+
+// for ( let i = 0 ; i<= 10 ; i = i + 2){
+//     console.log(i);
+// }
+
+//*********************************************************** */
+
+
+// for even odd number  print
+
+for ( let i = 0 ; i<= 10 ; i = i + 1){
+    console.log(i);
+}
+
+// for  odd number  print
+
+for ( let i = 1 ; i <= 10 ; i = i + 2){
+    console.log(i);
+}
+
+//*********************************************************** */
+
+for (let i = 0; i < 5; i++) {
+    console.log('print next-');
+    
+}
